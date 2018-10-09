e3b89fe538564d5228c8fa072eb0651d88e04ef4
Rukhsar Ahmad <itmolen1@gmail.com>
2018-10-09 13:59:13 +0500

    Revert "changes rukhsar"
    This reverts commit 84c63ba50dd6d99a8ee69c47f2000ee33d49f5da.

--- InvoiceDisk/App_Start/BundleConfig.cs
+++ InvoiceDisk/App_Start/BundleConfig.cs
@@ -9,11 +9,8 @@ namespace InvoiceDisk
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/jquery.validate.js",
-                        "~/Scripts/jquery.validate.unobtrusive.js",
-                        "~/Scripts/Datatable/jquery.dataTables.js",
-                        "~/Scripts/jquery-ui-1.12.1.min.js"));
+                        "~/Scripts/jquery-{version}.js"
+                       ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -24,16 +21,12 @@ namespace InvoiceDisk
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js",
-                      "~/Scripts/alertify.js"));
+                      "~/Scripts/bootstrap.js"
+                    ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css",
-                      "~/Content/alertifyjs/alertify.css",
-                      "~/Content/Datatable/css/jquery.dataTables.css",
-                      "~/Content/themes/base/jquery-ui.min.css"));
+                      "~/Content/bootstrap.css"
+                      ));
         }
     }
 }
